daf72fd7f3b2b44798ae5c719d3599e50d60038d
D'Bonis <dev@rodolfodebonis.com.br>
2025-03-11 07:29:33 -0300

    feat: Update package version to 0.1.1

--- microdetect/__init__.py
+++ microdetect/__init__.py
@@ -5,5 +5,5 @@ Este pacote fornece ferramentas para converter, anotar, aumentar, treinar
 e avaliar modelos de detecção de microorganismos em imagens de microscopia.
 """
 
-__version__ = "${VERSION_NUM}"
+__version__ = "0.1.1"
 __author__ = "MicroDetect Team"
