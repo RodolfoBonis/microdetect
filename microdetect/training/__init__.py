f6d3329ece410f3f3548d6eaf17f4abba91a09c1
D'Bonis <dev@rodolfodebonis.com.br>
2025-03-13 06:45:56 -0300

    test: Add comprehensive tests for various functionalities

--- microdetect/training/__init__.py
+++ microdetect/training/__init__.py
@@ -2,7 +2,7 @@
 Módulo de treinamento para MicroDetect.
 """
 
-from microdetect.training.evaluate import ModelEvaluator
+from microdetect.training.evaluate import ModelEvaluator, CrossValidator, ResourceMonitor, SpeedBenchmark
 from microdetect.training.train import YOLOTrainer
 
-__all__ = ["YOLOTrainer", "ModelEvaluator"]
+__all__ = ["YOLOTrainer", "ModelEvaluator", "CrossValidator", "ResourceMonitor", "SpeedBenchmark"]
